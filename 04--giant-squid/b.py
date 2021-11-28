2c3fd90cb11d5dc66e182f946cb7c00c362573e0
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-11-28 12:00:00 -0600

    04: Better parsing

--- 04--giant-squid/b.py
+++ 04--giant-squid/b.py
@@ -3,18 +3,17 @@ from util import findints, transpose
 
 
 def main():
-    calls = [59,91,13,82,8,32,74,96,55,51,19,47,46,44,5,21,95,71,48,60,68,81,80,14,23,28,26,78,12,22,49,1,83,88,39,53,84,37,93,24,42,7,56,20,92,90,25,36,34,52,27,50,85,75,89,63,33,4,66,17,98,57,3,9,54,0,94,29,79,61,45,86,16,30,77,76,6,38,70,62,72,43,69,35,18,97,73,41,40,64,67,31,58,11,15,87,65,2,10,99]
-
     f = open(sys.argv[1] if len(sys.argv) > 1 else 'in')
-    lines = [l.rstrip('\n') for l in f]
+    calls, *cardnums = f.read().strip().split('\n\n')
+    calls = findints(calls)
+
     cards = []
     curr = []
-    for line in lines:
-        if line:
+    for text in cardnums:
+        for line in text.strip().split('\n'):
             curr.append(findints(line))
-        else:
-            cards.append(Card(curr))
-            curr = []
+        cards.append(Card(curr))
+        curr = []
 
     won = set()
 
