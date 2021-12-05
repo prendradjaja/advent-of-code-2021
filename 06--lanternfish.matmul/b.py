532798ab9a7f869e03acc6d89bce9f857147c6b6
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-12-05 12:00:00 -0600

    06.matmul: Clean up comment a bit

--- 06--lanternfish.matmul/b.py
+++ 06--lanternfish.matmul/b.py
@@ -8,14 +8,16 @@ from matmul import matmul
 _ = 0  # So it's easy to see where the 1s are
 
 step = [
-    # Multiply the 1x9 vector (v) with a 9x9 matrix to get new 1x9 vector (w)
+    # If you multiply a 1x9 vector (v) with this 9x9 matrix, you get a new 1x9
+    # vector (w)
     #
     # Each column represents a new value (= column) in the new vector. Do some
     # example matrix multiplication to get an intuition for this.
     #
-    #* This column means w[1] = v[0]
-    #   * means w[2] = v[1]
-    #                  * means w[6] = v[0] + v[7]
+    #*-- This column means w[1] = v[0]
+    #|  *-- means w[2] = v[1]
+    #|  |              *-- means w[6] = v[0] + v[7]
+    #v  v              v
     [_, _, _, _, _, _, 1, _, 1],
     [1, _, _, _, _, _, _, _, _],
     [_, 1, _, _, _, _, _, _, _],
