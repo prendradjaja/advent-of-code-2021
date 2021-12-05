6dc7914e037c8a9c0eb7841c3879ae83ed0390b2
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-12-05 12:00:00 -0600

    one more fix without termcolor

--- untitled-11/s.py
+++ untitled-11/s.py
@@ -1,6 +1,7 @@
 import fileinput, collections, collections as cl, itertools, itertools as it, math, random, sys, re, string, functools
 from grid import gridsource as grid, gridcustom # *, gridsource, gridcardinal, gridplane
 from util import *
+from termcolor import colored, cprint
 
 
 def main():
@@ -12,18 +13,23 @@ def main():
             val = int(val)
             energy[(r, c)] = val
     flashes = 0
+    last_flashes = []
     def step():
-        nonlocal flashes
+        nonlocal flashes, last_flashes
+        last_flashes = []
         for pos in energy:
             energy[pos] += 1
         first = True
         shouldcontinue = False
+
+        # emulating a do {} while(shouldcontinue)
         while first or shouldcontinue:
             shouldcontinue = False
             first = False
             for pos in energy:
                 if energy[pos] >= 9:
-                    energy[pos] = 0
+                    energy[pos] = float('-inf')
+                    last_flashes.append(pos)
                     flashes += 1
                     for offset in grid.neivecs:
                         npos = grid.addvec(pos, offset)
@@ -31,9 +37,16 @@ def main():
                         if 0 <= r < 10 and 0 <= c < 10:
                             energy[(r, c)] += 1
                     shouldcontinue = True
-    for _ in range(1000):
+        for pos in last_flashes:
+            energy[pos] = 0
+    def show():
+        pass
+    # print('Before any steps:')
+    show()
+    for i in range(1000):
         step()
-        print(flashes)
+        # print(f'After step {i+1}:')
+        show()
     print(flashes)
 
 if __name__ == '__main__':
