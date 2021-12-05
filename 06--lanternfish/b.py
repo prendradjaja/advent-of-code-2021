13b039556a56f4e79d70aed981fa10ac5dbd3408
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-12-05 12:00:00 -0600

    06: Short and sweet

--- 06--lanternfish/b.py
+++ 06--lanternfish/b.py
@@ -4,28 +4,17 @@ from collections import Counter
 
 def main():
     f = open(sys.argv[1] if len(sys.argv) > 1 else 'in')
-    counter = Counter(int(n) for n in f.read().split(','))
-    counts = [counter[i] for i in range(9)]
 
-    def step():
-        nonlocal counts
-        births, *rest = counts
-        new_counts = [get(rest, i) for i in range(9)]
-        new_counts[6] += births
-        new_counts[8] += births
-        counts = new_counts
+    counts = [0] * 9
+    for n in f.read().split(','):
+        counts[int(n)] += 1
 
     for _ in range(256):
-        step()
+        births, *counts = counts + [0]
+        counts[6] += births
+        counts[8] += births
 
     print(sum(counts))
-
-
-def get(lst, idx):
-    if idx < len(lst):
-        return lst[idx]
-    else:
-        return 0
 
 
 if __name__ == '__main__':
