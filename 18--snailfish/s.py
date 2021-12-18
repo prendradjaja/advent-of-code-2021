092459ba42f327b5a265e9d8626975e97c7d1aa9
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-12-18 10:29:18 -0600

    explode works

--- 18--snailfish/s.py
+++ 18--snailfish/s.py
@@ -32,14 +32,53 @@ def reduce(sn):
 
 
 def maybe_explode(sn):
-    1/0  # not implemented
+    '''
+    >>> maybe_explode([[[[[9,8],1],2],3],4])
+    (True, [[[[0, 9], 2], 3], 4])
+    >>> maybe_explode([7,[6,[5,[4,[3,2]]]]])
+    (True, [7, [6, [5, [7, 0]]]])
+    >>> maybe_explode([[6,[5,[4,[3,2]]]],1])
+    (True, [[6, [5, [7, 0]]], 3])
+    >>> maybe_explode([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])
+    (True, [[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]])
+    >>> maybe_explode([[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]])
+    (True, [[3, [2, [8, 0]]], [9, [5, [7, 0]]]])
+
+    >>> maybe_explode( [7,[6,[5,[4,0]]]] )
+    (False, [7, [6, [5, [4, 0]]]])
+    >>> maybe_explode( [[6,[5,[4,0]]],1] )
+    (False, [[6, [5, [4, 0]]], 1])
+    >>> maybe_explode( [[3,[2,[1,0]]],[6,[5,[4,0]]]] )
+    (False, [[3, [2, [1, 0]]], [6, [5, [4, 0]]]])
+    '''
+    exploding_pair = find_4x_nested(sn)
+    if not exploding_pair:
+        return False, sn
+
+    explode_add(True, exploding_pair, sn)
+    explode_add(False, exploding_pair, sn)
+
+    parent, index = find_parent_and_index(exploding_pair, sn)
+    parent[index] = 0
+
+    return True, sn
+
+
+def explode_add(is_left, exploding_pair, sn):
+    find_neighbor = find_leftward_neighbor if is_left else find_rightward_neighbor
+    index_in_exploding = 0 if is_left else 1
+
+    neighbor = find_neighbor(exploding_pair, sn)
+    if neighbor:
+        value, parent, index_in_parent = neighbor
+        parent[index_in_parent] = value + exploding_pair[index_in_exploding]
 
 
 def maybe_split(sn):
     1/0  # not implemented
 
 
-# TODO this should be inorder traversal
+# i think this can be either preorder or inorder (this is preorder)
 def find_4x_nested(sn, depth=0, *, target=4):
     '''
     Find the leftmost 4x-nested pair inside SN, if any. Else, return None.
@@ -69,79 +108,97 @@ def find_4x_nested(sn, depth=0, *, target=4):
         return None
 
 
-def inorder_traversal(sn, parent):
+def find_parent_and_index(needle, haystack):
     '''
-    >>> for item, parent in inorder_traversal([1, 2], None):
+    Find NEEDLE in HAYSTACK (both snailfish numbers NOT regular numbers).
+    Return NEEDLE's parent and index in that parent.
+
+    >>> needle = [3, 4]
+    >>> find_parent_and_index(needle, [needle, [5, 6]])
+    ([[3, 4], [5, 6]], 0)
+    >>> find_parent_and_index(needle, [[2, needle], [5, 6]])
+    ([2, [3, 4]], 1)
+    '''
+    assert isinstance(needle, list)
+    for item, parent, index in inorder_traversal(haystack, None, None):
+        if needle is item:
+            return parent, index
+    1/0  # not found
+
+
+def inorder_traversal(sn, parent, index):
+    '''
+    >>> for item, parent, index in inorder_traversal([1, 2], None, None):
     ...     print(item)
     1
     [1, 2]
     2
     '''
     if isinstance(sn, list):
-        yield from inorder_traversal(sn[0], sn)
-    yield sn, parent
+        yield from inorder_traversal(sn[0], sn, 0)
+    yield sn, parent, index
     if isinstance(sn, list):
-        yield from inorder_traversal(sn[1], sn)
+        yield from inorder_traversal(sn[1], sn, 1)
 
 
-def right_to_left_traversal(sn, parent):
+def right_to_left_traversal(sn, parent, index):
     '''
     This is just the reverse of an inorder traversal.
 
-    >>> for item, parent in right_to_left_traversal([1, 2], None):
+    >>> for item, parent, index in right_to_left_traversal([1, 2], None, None):
     ...     print(item)
     2
     [1, 2]
     1
     '''
     if isinstance(sn, list):
-        yield from right_to_left_traversal(sn[1], sn)
-    yield sn, parent
+        yield from right_to_left_traversal(sn[1], sn, 1)
+    yield sn, parent, index
     if isinstance(sn, list):
-        yield from right_to_left_traversal(sn[0], sn)
+        yield from right_to_left_traversal(sn[0], sn, 0)
 
 
 def find_rightward_neighbor(pair, sn):
     '''
-    Find the first regular number to the right of PAIR inside SN, if any.
-    Else, return None.
+    Find the first regular number (and its parent & index in that parent) to
+    the right of PAIR inside SN, if any. Else, return None.
 
     >>> pair = [1, 2]
     >>> find_rightward_neighbor(pair, [pair, [3, 4]])
-    (3, [3, 4])
+    (3, [3, 4], 0)
     >>> find_rightward_neighbor(pair, [pair, 3])
-    (3, [[1, 2], 3])
+    (3, [[1, 2], 3], 1)
     >>> find_rightward_neighbor(pair, [[9, 9], pair])
     '''
     assert isinstance(pair, list)
     found = False
-    for item, parent in inorder_traversal(sn, None):
+    for item, parent, index in inorder_traversal(sn, None, None):
         if pair is item:
             found = True
         elif found and isinstance(item, int) and parent is not pair:
-            return item, parent
+            return item, parent, index
     return None
 
 
 def find_leftward_neighbor(pair, sn):
     '''
-    Find the first regular number to the right of PAIR inside SN, if any.
-    Else, return None.
+    Find the first regular number (and its parent & index in that parent) to
+    the right of PAIR inside SN, if any. Else, return None.
 
     >>> pair = [8, 9]
     >>> find_leftward_neighbor(pair, [[6, 7], pair])
-    (7, [6, 7])
+    (7, [6, 7], 1)
     >>> find_leftward_neighbor(pair, [7, pair])
-    (7, [7, [8, 9]])
+    (7, [7, [8, 9]], 0)
     >>> find_leftward_neighbor(pair, [pair, [1, 1]])
     '''
     assert isinstance(pair, list)
     found = False
-    for item, parent in right_to_left_traversal(sn, None):
+    for item, parent, index in right_to_left_traversal(sn, None, None):
         if pair is item:
             found = True
         elif found and isinstance(item, int) and parent is not pair:
-            return item, parent
+            return item, parent, index
     return None
 
 
