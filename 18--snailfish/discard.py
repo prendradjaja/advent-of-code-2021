537c2efa951522bfd56715e7a2cbe74252e91fb2
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-12-18 10:37:08 -0600

    discard++

--- 18--snailfish/discard.py
+++ 18--snailfish/discard.py
@@ -14,3 +14,42 @@ def find_rightward_neighbor(pair, sn, *, _pair_found=False):
         or find_rightward_neighbor(pair, right, _pair_found=_pair_found)
         or None
     )
+
+
+def to_variables(snlist):
+    '''
+    >>> snlist = """
+    ... [3, [1, 4]]
+    ... [1, 5]
+    ... """.strip()
+    >>> to_variables(snlist)
+    {-1: 3, -2: 1, -3: 4, -4: 1, -5: 5}
+    '''
+    variables = {}
+    for line in snlist.split('\n'):
+        line = line.strip()
+        sn = safer_eval(line)
+        to_variables_from_sn(sn, variables)
+    return variables
+
+
+def to_variables_from_sn(sn, variables):
+    if isinstance(sn, int):
+        name = next_variable(variables)
+        variables[name] = sn
+    elif isinstance(sn, list):
+        left, right = sn
+        to_variables_from_sn(left, variables)
+        to_variables_from_sn(right, variables)
+    else:
+        1/0  # invalid
+
+
+def next_variable(variables):
+    '''
+    >>> next_variable({})
+    -1
+    >>> next_variable({-1: 3})
+    -2
+    '''
+    return -len(variables) - 1
